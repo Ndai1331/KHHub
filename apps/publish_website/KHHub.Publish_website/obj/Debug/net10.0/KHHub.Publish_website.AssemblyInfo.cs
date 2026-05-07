014a32ccb1d378fb9f0a6cca182fc0f0c89cff4e
ezio02912 <167726829+ezio02912@users.noreply.github.com>
2026-05-07 17:07:02 +0700

    [gateways/web] update setting json

--- apps/publish_website/KHHub.Publish_website/obj/Debug/net10.0/KHHub.Publish_website.AssemblyInfo.cs
+++ apps/publish_website/KHHub.Publish_website/obj/Debug/net10.0/KHHub.Publish_website.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KHHub.Publish_website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+317065b3d0176e1cdafc06d5d44ad25d1a27a860")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bd80c09a976789ceda4792c014dbff389e80576")]
 [assembly: System.Reflection.AssemblyProductAttribute("KHHub.Publish_website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KHHub.Publish_website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
